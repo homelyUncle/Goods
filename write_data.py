95751bed6940062af89e5ab73730a960711b6655
Mike <mikhail.shkliaev@live.ru>
2020-11-18 16:05:09 +0300

    Add files via upload
    исправил ошибку, вызванную использованием рекурсии

--- write_data.py
+++ write_data.py
@@ -5,25 +5,26 @@ def add_line():
         name = input('Input type of belts: ')
         if name == '':  # ...исключая пустые строки...
             print("\t\tYou didn't write anything")
-            input_type()
+            name = input_type()
         elif len(name) > 16:  # ...и исключая названия длиннее
             print('\t\tThe name you entered is too long')
-            input_type()
+            name = input_type()
         return name
 
     # постоянный запрос количества до получения корректного значения...
     def input_nums():
-        count_int = 0
         count = input('Input count of belts: ')
         try:
-            count_int = int(count)
+            count = int(count)
         except ValueError:  # ...исключая возможность ввода литер
             print('\t\tYou entered not an integer')
-            input_nums()
-        return count_int
+            count = input_nums()
+        return count
 
     type_of_belt = input_type()
     count_of_belt = input_nums()
     ret = {type_of_belt: count_of_belt}
     # возвращем данные (key: str, value: int)
     return ret
+
+print(add_line())
