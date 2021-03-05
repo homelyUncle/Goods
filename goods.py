8feb66c675436fd46fa024498b4024834cc2e0d2
Mike <mikhail.shkliaev@live.ru>
2021-03-05 10:19:19 +0300

    Update goods.py
    оптимизировал визуализацию и обработку команд

--- goods.py
+++ goods.py
@@ -36,7 +36,7 @@ def find(all_items, item_to_find):
                 founded_index = [all_items.index(i)]
             else:
                 founded_index.append(all_items.index(i))
-            out = f"\t{ct + 1}\t{i['name']}\t{i['count']}"
+            out = f"\t{ct + 1}\t{i['name']:15}\t{i['count']:3}"
             print(out)
         else:
             continue
@@ -63,14 +63,13 @@ def show_all(all_items):
     for i in range(len(all_items)):
         j = all_items[i]
         if j['count'] == 0:
-            warn = '\t-- ТОВАРА НЕТ НА СКЛАДЕ!!! --'
+            warn = '-- ТОВАРА НЕТ НА СКЛАДЕ!!! --'
         elif j['count'] < 2:
-            warn = '\t-- ПОСЛЕДНИЙ!!! --'
         elif j['count'] < 3:
-            warn = '\t-- ПОСЛЕДНИЕ!!! --'
+            warn = '-- ПОСЛЕДНИЕ!!! --'
         else:
             warn = ''
-        print(f"\t{i + 1}\t{j['name']} ____ {j['count']} {warn}")
+        print(f"{(i + 1)}\t{j['name']:15} ===> {j['count']:3} {warn}")
 
 
 def add():
@@ -121,9 +120,9 @@ def main(file_path):  # запуск программы
     try:
         main_list = read_from_file(file_path)  # попытка чтения файла и записи данных в переменную
     except FileNotFoundError:  # создаём новый файл
-        print('\n\t- - ФАЙЛ С ДАННЫМИ ОТСУТСВУЕТ - -\n')
+        print('\t- - ФАЙЛ С ДАННЫМИ ОТСУТСВУЕТ - -\n')
         to_make_file = input('Хотите добавить запись? (y/n)\n')
-        if to_make_file == 'y' or to_make_file == 'Y' or to_make_file == 'н' or to_make_file == 'Н':
+        if to_make_file in ('y', 'Y', 'н', 'Н'):
             main_list = [add()]
             write_to_file(FILE_PATH, main_list)
         else:
@@ -132,61 +131,59 @@ def main(file_path):  # запуск программы
     while True:
         print()
         main_menu = input("-= Главное меню =-"  # типа меню
-                          "\n\t's' показать все товары"
-                          "\t\t'i' показать один товар"
-                          "\n\t'z' показать отсутствующее"
-                          "\t'a' добавить товар в список"
-                          "\n\t'c' очистить экран"
-                          "\t\t'q' выйти из программы\n")
+                          f"\n\t's' показать все товары\t\t'i' показать один товар"
+                          "\n\t'z' показать отсутствующее\t'a' добавить товар в список"
+                          "\n\t'c' очистить экран\t\t'q' выйти из программы\n")
 
-        if main_menu == 's' or main_menu == 'S' or main_menu == 'ы' or main_menu == 'Ы':  # вывод всего списка
+        if main_menu in ('s', 'S', 'ы', 'Ы'):  # вывод всего списка
             clear_screen()
             show_all(main_list)
 
-        elif main_menu == 'a' or main_menu == 'A' or main_menu == 'ф' or main_menu == 'Ф':  # добавление записи
+        elif main_menu in ('a', 'A', 'ф', 'Ф'):  # добавление записи
             clear_screen()
             main_list.append(add())
             write_to_file(FILE_PATH, main_list)
 
-        elif main_menu == 'i' or main_menu == 'I' or main_menu == 'ш' or main_menu == 'Ш':
+        elif main_menu in ('i', 'I', 'ш', 'Ш'):
             # вывод информации об одной записи
             finding_item = input('введите название/часть названия товара, который нужно найти: ')
             index_changing_count = main_list[find(main_list, finding_item.lower())]
             print()
             choose_action = input("выберите действие:"
-                                  "\n\t'с' изменить количество"
-                                  "\t\t'n' изменить название"
-                                  "\n\t'd' удалить запись"
-                                  "\t\t'm' вернуться в 'главное меню'\n")
-            if choose_action == 'c' or choose_action == 'C' or choose_action == 'с' or choose_action == 'С':
+                                  "\n\t'с' изменить количество\t\t'n' изменить название"
+                                  "\n\t'd' удалить запись\t\t'm' вернуться в 'главное меню'\n")
+            if choose_action in ('c', 'C', 'с', 'С'):
                 new_count = input_nums()
                 index_changing_count['count'] = new_count
                 print(f"\n\t{index_changing_count['name']} ____ {index_changing_count['count']}\n")
                 write_to_file(FILE_PATH, main_list)
 
-            if choose_action == 'n' or choose_action == 'N' or choose_action == 'т' or choose_action == 'Т':
+            if choose_action in ('n', 'N', 'т', 'Т'):
                 new_name = input_type()
                 index_changing_count['name'] = new_name
                 print(f"\n\t{index_changing_count['name']} ____ {index_changing_count['count']}\n")
                 write_to_file(FILE_PATH, main_list)
-            if choose_action == 'd' or choose_action == 'D' or choose_action == 'в' or choose_action == 'В':
+
+            if choose_action in ('d', 'D', 'в', 'В'):
                 main_list.remove(index_changing_count)
                 write_to_file(FILE_PATH, main_list)
-            if choose_action == 'm' or choose_action == 'M' or choose_action == 'ь' or choose_action == 'Ь':
+
+            if choose_action in ('m', 'M', 'ь', 'Ь'):
                 clear_screen()
                 continue
-        elif main_menu == 'z' or main_menu == 'Z' or main_menu == 'я' or main_menu == 'Я':
+
+        elif main_menu in ('z', 'Z', 'я', 'Я'):
             # вывод записей с нулевыми значениями
-            print('\n\t-- ТОВАР, ОТСУТСТВУЮЩИЙ НА СКЛАДЕ --')
+            print('\t-- ТОВАР, ОТСУТСТВУЮЩИЙ НА СКЛАДЕ --')
             for i in main_list:
                 if i['count'] == 0:
                     print(f"\t\t{i['name']}")
 
-        elif main_menu == 'q' or main_menu == 'Q' or main_menu == 'й' or main_menu == 'Й':  # закрытие программы
+        elif main_menu in ('q', 'Q', 'й', 'Й'):  # закрытие программы
             write_to_file(FILE_PATH, main_list)
             end()
 
-        elif main_menu == 'c' or main_menu == 'C' or main_menu == 'с' or main_menu == 'С':  # очистка экрана
+        elif main_menu in ('c', 'C', 'с', 'С'):  # очистка экрана
             clear_screen()
 
 
